82caef0e04b7efac7269aecd1cbe17f2a2a2ae27
PatchOfScotland <d.marchant@ed-alumni.net>
2020-07-20 11:33:51 +0200

    container now claims ownership of mounted directories

--- scripts/docker-entry.sh
+++ scripts/docker-entry.sh
@@ -6,7 +6,10 @@ service cron restart
 # Needed to update shell for oci-cli
 service uwsgi_mcweb restart
 
-## Collect initial instruments
+# Take ownership of mounted directories
+chown -R www-data:www-data /srv/mcweb/McWeb/mcsimrunner/sim /srv/mcweb/McWeb/mcsimrunner/static/data
+
+# Collect initial instruments
 sudo -u www-data /srv/mcweb/McWeb/scripts/update_instr.sh
 
 # Hack to keep this image running indefinitely
